e0fb2b80ff2f07ad8abedf9655cf81a2d3a68967
Alan Ruiz <alan.c.ruiz.70@gmail.com>
2025-07-15 10:09:29 -0600

    actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'estructurales';
+
+  isVisible:boolean=true;
+
+  toggle():void{
+    this.isVisible=!this.isVisible;
+  }
+
+  /*********** */
+
+  lenguajes:string[]=['C/C++', 'Java', 'Phyton', 'Javascript']
+  nuevo:string='';
+
+  addItem():void{
+    this.lenguajes.push(this.nuevo);
+    this.nuevo=''; /*regresa la variable en blanco*/
+  }
+
+  /************* */
+
+  selectedOption:string='default';
+
 }
